419cb3e616cc0ad0209be0d1f4898be67d253133
Venkatesulu Valagonda <venkatesulu.valagonda@libertymutual.com>
2019-03-15 14:39:36 -0400

    Add files via upload

---
+++ message-receiver.js
@@ -0,0 +1,57 @@
+const AWS = require("aws-sdk");
+
+if (!AWS.config.region) {
+  AWS.config.update({
+    region: "us-east-1"
+  });
+}
+
+const ses = new AWS.SES();
+
+exports.handler = (event, context, callback) => {
+  const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+          message: 'SQS event processed.',
+          input: event,
+      }),
+  };
+  
+  console.log("data: ", JSON.stringify(event));
+
+  const { name, email, message } = JSON.parse(event.Records[0].body);
+  const emailParams = {
+    Source: 'venkatesulu.valagonda@libertymutual.com',
+    Destination: {
+      ToAddresses: ['venkatesulu.valagonda@libertymutual.com'],
+    },
+    ReplyToAddresses: [email],
+    Message: {
+      Body: {
+        Text: {
+          Charset: 'UTF-8',
+          Data: `Name: ${name}<p/> Email: ${email}<p/> Message: ${message}`
+        }
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: "Certainly Customer Inquiry"
+      }
+    }
+  };
+
+  console.log("emailParams: ", JSON.stringify(emailParams));
+
+  ses.sendEmail(emailParams, (err, data) => {
+    if (err) {
+      response.statusCode = 402
+      response.body.message = `${err} ${err.stack}`
+    }
+    if (data) {
+      response.body.message = 'SQS Event processed, Data:' + data
+      console.log(data);
+    }
+  });
+
+  callback(null, response);
+};
---
+++ message-sender.js
@@ -0,0 +1,44 @@
+var AWS = require('aws-sdk');
+var sqs = new AWS.SQS({
+    region: 'us-east-1'
+});
+
+exports.handler = function(event, context, callback) {
+    var accountId = context.invokedFunctionArn.split(":")[4];
+    console.log('Account Id: ' + accountId );
+
+    var queueUrl = 'https://sqs.us-east-1.amazonaws.com/' + accountId + '/A0266185Queue';
+
+    // response and status of HTTP endpoint
+    var responseBody = {
+        message: ''
+    };
+    var responseCode = 200;
+
+    // SQS message parameters
+    var params = {
+        MessageBody: event.body,
+        QueueUrl: queueUrl
+    };
+
+    sqs.sendMessage(params, function(err, data) {
+        if (err) {
+            console.log('error:', "failed to send message" + err);
+            responseBody.message = err;
+            var responseCode = 500;
+        } else {
+            console.log('data:', data.MessageId);
+            responseBody.message = 'Sent to ' + queueUrl;
+            responseBody.messageId = data.MessageId;
+        }
+        var response = {
+            statusCode: responseCode,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(responseBody)
+        };
+
+        callback(null, response);
+    });
+}
